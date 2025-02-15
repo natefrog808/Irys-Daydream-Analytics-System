2a325526b16e529c7e43b25db41e987a5f1a1bd7
natefrog808 <wetakatini@gmail.com>
2025-02-15 13:16:22 -1000

    Create integration.ts

---
+++ src/blockchain/integration.ts
@@ -0,0 +1,5 @@
+class BlockchainManager {
+  private readonly irysClient: IrysClient;
+  private readonly verificationEngine: VerificationEngine;
+  private readonly storageOptimizer: StorageOptimizer;
+}
